f32ca224ef8a6a6c4ab0dba59eddad45f1ec96bc
ivan99599 <149070855+ivan99599@users.noreply.github.com>
2023-10-30 16:31:12 +0200

    updated test.py

--- test.py
+++ test.py
@@ -1,5 +1,7 @@
-my_name = Ivan
+my_name = "Ivan"
 my_age = 22 
 
-if my age == 22:
-print(f"My name is {my_name} and I am {my_age} years old")
+if my_age == 22:
+    print(f"My name is {my_name} and I am {my_age} years old")
+else:
+    print("End")
